6a4939965613ade15069c5246315752d1dc47bce
Pablo Ganuza Vidal <pabloganuza@gmail.com>
2019-10-13 18:12:20 +0200

    Gather all sucless utilities together.

--- Repositorios/suckless/dwm/config.h
+++ Repositorios/suckless/dwm/config.h
@@ -58,7 +58,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+const char term[] = "st";
+static const char *termcmd[]  = { term, NULL };
+static const char *rangercmd[]  = { term, "ranger", NULL };
 static const char *slockcmd[]  = { "slock", NULL };
 static const char *clipmenucmd[]  = { "clipmenu", NULL };
 
@@ -68,6 +70,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slockcmd } },
 	{ MODKEY|ShiftMask,             XK_h,      spawn,          {.v = clipmenucmd } },
+	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = rangercmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
